b7ee7eb0ed572196e2fdaa13397a70110c024084
appsdaroi <n30nyt@gmail.com>
2023-12-24 22:27:21 -0300

    chore: clean project

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Felipe Boni | Personal Portfolio",
-  description: "Felipe Boni is a full-stack developer with 8 years of experience.",
+  description: "Felipe Boni is a full-stack developer with 4 years of experience.",
 };
 
 export default function RootLayout({
